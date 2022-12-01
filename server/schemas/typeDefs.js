2d0e487323db341a323b537d8b9e143b1b99ce52
banjosquash <banjosquash@gmail.com>
2022-12-01 09:07:06 -0500

    set up file structure worked on implementing type defs and resolvers in back end

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,53 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+const typeDefs =gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    savedBooks: String
+
+}
+
+type Book {
+    _id: ID
+    authors: [String]
+    description: [String]
+    bookId: String
+    image: String
+    link: String
+    title: String
+}
+
+type Query {
+    me: User
+}
+
+input SavedBookInput {
+  authors: [String]
+  description: String
+  bookId: String
+  image: String
+  forSale: String
+  link: String
+  title: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: SavedBookInput): User
+    deleteBook(bookId: String!): User
+  }
+
+`;
+
+
+
+module.exports = typeDefs;
